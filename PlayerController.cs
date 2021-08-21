b7f4e2a64261ac912fa313e7e7312d21478d4ae0
Omokorede Adeyeye <lordkratos365@gmail.com>
2021-08-21 13:58:24 +0100

    usingVector3

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,14 +7,14 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tra
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Move vehicle foward
-        // transform.Translate(0, 0, 1);
+        transform.Translate(0, 0, 1);
         transform.Translate(Vector3.foward);
         
     }
