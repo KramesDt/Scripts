002cca0b52b9dccc5edb721df7ac9ca4301dd6ec
Omokorede Adeyeye <lordkratos365@gmail.com>
2021-08-21 13:56:36 +0100

    initialCommit

---
+++ PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        tra
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move vehicle foward
+        // transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.foward);
+        
+    }
+}
